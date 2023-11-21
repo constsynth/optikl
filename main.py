be5e7dfee475233044f4be9dd490d95df98436d5
constsynth <constsynth@gmail.com>
2023-11-21 21:59:41 +0700

    mobilenet for image embeddings added, requirements are updated

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 Hello! This is constantly evolving image processing library with many useful methods.
 Most common examples of using are presented below:
 """
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-from optikl.image_processing import find_face, crop_center, represent, super_resolution, image_similarity
-print(image_similarity('im2.jpg', 'im1.jpg', type_of_encoding='conv'))
+
+from optikl.image_processing import represent, image_similarity
+print(image_similarity('_92607945_melanoma.jpg', 'photo_2023-11-16_14-54-31.jpg', type_of_encoding='conv', use_features=True))
+
+# print(represent('_92607945_melanoma.jpg'))
+
--- optikl/image_processing.py
+++ optikl/image_processing.py
@@ -9,6 +9,8 @@ from optikl.model.tools import Fundamental
 import torch
 import cv2
 import matplotlib.pyplot as plt
+import skimage
+import mediapipe as mp
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
@@ -56,14 +58,48 @@ def super_resolution(image_path: str = None, save: bool = False) -> Image.Image:
     return image
 
 def represent(path_to_image: str = None, to_vector: bool = False, to_tensor: bool = True, type: str = 'linear'):
-    print(device)
-    method = Fundamental().to(device)
-    return method.encode(path_to_image=path_to_image, to_vector=to_vector,to_tensor=to_tensor, type=type)
+    # print(device)
+    # method = Fundamental().to(device)
+    # return method.encode(path_to_image=path_to_image, to_vector=to_vector,to_tensor=to_tensor, type=type)
 
-def image_similarity(im1_path: str = None, im2_path: str = None, type_of_encoding: str = 'linear') -> float:
+    if not os.path.exists("./optikl/models/weights/mobilenet"):
+        os.makedirs('./optikl/models/weights/mobilenet', exist_ok=True)
+        print('MobileNetV3 was not found, downloading...')
+        url = 'https://storage.googleapis.com/mediapipe-tasks/image_embedder/mobilenet_v3_small_075_224_embedder.tflite'
+        response = requests.get(url, stream=True)
+        open('./optikl/models/weights/mobilenet/mobilenet_v3_small_075_224_embedder.tflite', 'wb').write(response.content)
+        print('MobileNetV3 has downloaded succesfully!')
+    BaseOptions = mp.tasks.BaseOptions
+    ImageEmbedder = mp.tasks.vision.ImageEmbedder
+    ImageEmbedderOptions = mp.tasks.vision.ImageEmbedderOptions
+    VisionRunningMode = mp.tasks.vision.RunningMode
+
+    options = ImageEmbedderOptions(
+        base_options=BaseOptions(model_asset_path='./optikl/models/weights/mobilenet/mobilenet_v3_small_075_224_embedder.tflite'),
+        quantize=True,
+        running_mode=VisionRunningMode.IMAGE)
+
+    with ImageEmbedder.create_from_options(options) as embedder:
+        mp_image = mp.Image.create_from_file(path_to_image)
+        embedding_result = embedder.embed(mp_image)
+    return embedding_result.embeddings[0]
+
+
+def image_similarity(im1_path: str = None, im2_path: str = None, type_of_encoding: str = 'linear', size = (512, 512), use_features: bool = False) -> float:
+    ImageEmbedder = mp.tasks.vision.ImageEmbedder
     print(device)
-    method = Fundamental().to(device)
-    return method.distance(im1_path=im1_path, im2_path=im2_path, type_of_encoding=type_of_encoding)
+    if use_features:
+        im1 = represent(im1_path)
+        im2 = represent(im2_path)
+        similarity = ImageEmbedder.cosine_similarity(im1, im2)
+    else:
+        im1 =cv2.resize(cv2.imread(im1_path), size)
+        im2 = cv2.resize(cv2.imread(im2_path), size)
+        similarity = skimage.metrics.structural_similarity(im1, im2, channel_axis=-1)
+    # method = Fundamental().to(device)
+    # return method.distance(im1_path=im1_path, im2_path=im2_path, type_of_encoding=type_of_encoding)
+    return similarity
+
 
 def find_face(img_path: str = None, show: bool = False, save: bool = True) -> dict:
     image = cv2.imread(img_path)
